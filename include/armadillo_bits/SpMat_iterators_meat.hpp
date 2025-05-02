4d9a82294a4f2f3e3dbad6db5510f33c8606454a
conrad <conrad@localhost.localdomain>
2025-05-02 14:14:07 +1000

    issue compile time warning for unused outputs

--- include/armadillo_bits/SpMat_iterators_meat.hpp
+++ include/armadillo_bits/SpMat_iterators_meat.hpp
@@ -62,7 +62,7 @@ SpMat<eT>::iterator_base::iterator_base(const SpMat<eT>& in_M, const uword in_co
 
 
 template<typename eT>
-arma_inline
+inline
 eT
 SpMat<eT>::iterator_base::operator*() const
   {
--- include/armadillo_bits/SpSubview_iterators_meat.hpp
+++ include/armadillo_bits/SpSubview_iterators_meat.hpp
@@ -269,7 +269,7 @@ SpSubview<eT>::const_iterator::const_iterator(const const_iterator& other)
 
 
 template<typename eT>
-arma_inline
+inline
 eT
 SpSubview<eT>::const_iterator::operator*() const
   {
