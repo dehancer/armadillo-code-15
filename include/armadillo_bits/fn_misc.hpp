ef46b50c0c7df43a6e7657b3cfcf201162a356f8
conrad <conrad@localhost.localdomain>
2025-05-07 12:56:51 +1000

    deprecate stand-alone is_finite()

--- include/armadillo_bits/fn_misc.hpp
+++ include/armadillo_bits/fn_misc.hpp
@@ -149,8 +149,8 @@ logspace(const double A, const double B, const uword N = 50u)
 
 //! kept for compatibility with old user code
 template<typename eT>
-arma_warn_unused
-arma_inline
+arma_frown("use std::isfinite(val) instead")
+inline
 bool
 is_finite(const eT x, const typename arma_scalar_only<eT>::result* junk = nullptr)
   {
@@ -163,7 +163,7 @@ is_finite(const eT x, const typename arma_scalar_only<eT>::result* junk = nullpt
 
 //! kept for compatibility with old user code
 template<typename T1>
-arma_warn_unused
+arma_frown("use X.is_finite() instead")
 inline
 bool
 is_finite(const Base<typename T1::elem_type,T1>& X)
@@ -177,7 +177,7 @@ is_finite(const Base<typename T1::elem_type,T1>& X)
 
 //! kept for compatibility with old user code
 template<typename T1>
-arma_warn_unused
+arma_frown("use X.is_finite() instead")
 inline
 bool
 is_finite(const SpBase<typename T1::elem_type,T1>& X)
@@ -191,7 +191,7 @@ is_finite(const SpBase<typename T1::elem_type,T1>& X)
 
 //! kept for compatibility with old user code
 template<typename T1>
-arma_warn_unused
+arma_frown("use X.is_finite() instead")
 inline
 bool
 is_finite(const BaseCube<typename T1::elem_type,T1>& X)
