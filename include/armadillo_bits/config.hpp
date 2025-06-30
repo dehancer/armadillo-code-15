9843393a7ab8e83f00fc1129f9da7de98d134245
conrad <conrad@localhost.localdomain>
2025-06-30 19:18:03 +1000

    cleanup

--- include/armadillo_bits/config.hpp
+++ include/armadillo_bits/config.hpp
@@ -319,9 +319,6 @@
 #if defined(ARMA_BLAS_LONG) || defined(ARMA_BLAS_LONG_LONG)
   #undef  ARMA_BLAS_64BIT_INT
   #define ARMA_BLAS_64BIT_INT
-  
-  // #pragma message ("options ARMA_BLAS_LONG and ARMA_BLAS_LONG_LONG are deprecated;")
-  // #pragma message ("use ARMA_BLAS_64BIT_INT instead")
 #endif
 
 #if defined(ARMA_DONT_OPTIMISE_BAND) || defined(ARMA_DONT_OPTIMISE_SOLVE_BAND)
